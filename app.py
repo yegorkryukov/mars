a9a5475b47b8d21c7754da15459f27aec3d6fbd6
Yegor Kryukov <yegor.kryukov@gmail.com>
2018-05-25 18:25:49 -0400

    Added "scrape" route to main app

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
 from flask import Flask
 from datetime import datetime
+from scrape_mars import scrape1
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
 
     return """
-    <h1>Hello heroku</h1>
-    <p>It is currently {time}.</p>
+    <h1>Main page to Mars</h1>
+    """
 
-    <img src="http://loremflickr.com/600/400">
-    """.format(time=the_time)
+
+@app.route('/scrape')
+def do_scrape(): 
+    results = scrape1()
+    return str(results)
+
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
